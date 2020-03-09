83636785eee34d7826080fdc8acb9b17817ec75f
ngnawen <ngnawens@gmail.com>
2020-03-09 03:03:35 +0100

    Second commit

--- src/main/java/cm/belrose/helloworld/Hello.java
+++ src/main/java/cm/belrose/helloworld/Hello.java
@@ -12,5 +12,6 @@ package cm.belrose.helloworld;
 public class Hello {
     public static void main(String ...args){
         System.out.println("Hello world.........");
+        System.out.println("Hello world 1111111111111111.........");
     }
 }
